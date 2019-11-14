12eeeb45c6eaf2814426490bb2d48885b30e76b0
supertinypeaunt <1176032017@qq.com>
2019-11-14 22:21:20 +0800

    后台管理-头部组件完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,17 +1,22 @@
 <template>
   <el-container>
   <el-aside width="200px">
+    <!-- 侧边栏组件 -->
     <home-aside></home-aside>
   </el-aside>
   <el-container>
-    <el-header>Header</el-header>
-    <el-main>Main</el-main>
+    <el-header>
+      <!-- 头部组件 -->
+      <home-header></home-header>
+    </el-header>
+    <el-main class="main">Main</el-main>
   </el-container>
 </el-container>
 </template>
 
 <script>
 import Aside from './Aside'
+import Header from './Header'
 
 export default {
   created () {
@@ -26,11 +31,15 @@ export default {
     }
   },
   components: {
-    'home-aside': Aside
+    'home-aside': Aside,
+    'home-header': Header
   }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.main{
+  height: 100%;
+  background-color: #f2f3f5;
+}
 </style>
