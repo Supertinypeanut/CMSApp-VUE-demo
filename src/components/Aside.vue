cefad34b3f1df71ee953ab06adf0a996337732f7
supertinypeaunt <1176032017@qq.com>
2019-11-15 13:30:12 +0800

    后台管理-侧边栏样式优化

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -4,37 +4,33 @@
           <img src="../assets/img/logo_index.png" alt="">
       </div>
       <el-menu
-      default-active="2"
+      default-active="1"
       class="el-menu-vertical-demo"
       background-color="#323745"
       text-color="#ADAFB5"
-      active-text-color="#409eff;">
-       <el-menu-item>
-      <router-link slot="title" to="/home" index="4">
-      首页</router-link>
-      </el-menu-item>
+      active-text-color="#fff"
+      router
+      >
+       <el-menu-item index="1" route="/home">首页</el-menu-item>
       <el-submenu index="2">
         <template slot="title">
           <span>内容管理</span>
         </template>
-        <el-menu-item><router-link slot="title" to="/content/publish" index="2-1">发布文章</router-link></el-menu-item>
-        <el-menu-item><router-link slot="title" to="/content/articles" index="2-2">内容列表</router-link></el-menu-item>
-        <el-menu-item><router-link slot="title" to="/content/comment" index="2-3">评论管理</router-link></el-menu-item>
-        <el-menu-item><router-link slot="title" to="/content/material" index="2-4">素材管理</router-link></el-menu-item>
+        <el-menu-item index="2-1" route="/content/publish">发布文章</el-menu-item>
+        <el-menu-item index="2-2" route="/content/articles">内容列表</el-menu-item>
+        <el-menu-item index="2-3" route="/content/comment">评论管理</el-menu-item>
+        <el-menu-item index="2-4" route="/content/material">素材管理</el-menu-item>
       </el-submenu>
       <el-submenu index="3">
         <template slot="title">
           <span>粉丝管理</span>
         </template>
-        <el-menu-item><router-link slot="title" to="/fans/data" index="3-1">图文数据</router-link></el-menu-item>
-        <el-menu-item><router-link slot="title" to="/fans/survey" index="3-2">粉丝概况</router-link></el-menu-item>
-        <el-menu-item><router-link slot="title" to="/fans/portrait" index="3-3">粉丝画像</router-link></el-menu-item>
-        <el-menu-item><router-link slot="title" to="/fans/lists" index="3-4">粉丝列表</router-link></el-menu-item>
+        <el-menu-item index="3-1" route="/fans/data">图文数据</el-menu-item>
+        <el-menu-item index="3-2" route="/fans/survey">粉丝概况</el-menu-item>
+        <el-menu-item index="3-3" route="/fans/portrait">粉丝画像</el-menu-item>
+        <el-menu-item index="3-4" route="/fans/lists">粉丝列表</el-menu-item>
       </el-submenu>
-      <el-menu-item>
-      <router-link slot="title" to="/account" index="4">
-      账户信息</router-link>
-      </el-menu-item>
+      <el-menu-item index="4" route="/account">账户信息</el-menu-item>
     </el-menu>
   </div>
 </template>
@@ -62,6 +58,9 @@ export default {
     // 剔出导航边框一像素
     .el-menu-vertical-demo.el-menu{
         border: 0;
+        .el-menu-item.is-active{
+        background-color: rgb(40, 162, 202)!important;
+      }
     }
 }
 </style>
