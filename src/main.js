3bf43bb8a069fb14896f9294f9540d3e18a52ea8
supertinypeaunt <1176032017@qq.com>
2019-11-16 13:06:19 +0800

    后台管理-json-big解决删除文章id过大js限制

--- src/main.js
+++ src/main.js
@@ -11,6 +11,9 @@ import './style/index.less'
 import '../node_modules/nprogress/nprogress.js'
 import '../node_modules/nprogress/nprogress.css'
 
+// json-bigint
+import JSONbig from 'json-bigint'
+
 import axios from 'axios'
 
 // 初始化ElementUI组件库
@@ -21,6 +24,17 @@ Vue.config.productionTip = false
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
 // 设置请求头携带token
 axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`
+// 处理响应参数-js最大数限制
+axios.defaults.transformResponse = [
+  function (data) {
+    try {
+      data = JSONbig.parse(data)
+      return data
+    } catch (error) {
+      return {}
+    }
+  }
+]
 
 // 挂载axios到实例
 Vue.prototype.$axios = axios
