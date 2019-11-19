4db5eace222a45ca158ff51f6b4a8baf41bf0bf8
supertinypeaunt <1176032017@qq.com>
2019-11-19 22:17:24 +0800

    后台管理-内容管理素材上传

--- src/views/home/account/index.vue
+++ src/views/home/account/index.vue
@@ -1,13 +1,95 @@
 <template>
-  <h1>账户信息</h1>
+  <el-card class="box-card">
+  <div slot="header" class="clearfix">
+    <span>账号信息</span>
+    <el-button style="float:right;" @click="onEdit" type="primary" icon="el-icon-edit" circle></el-button>
+  </div>
+  <el-row>
+    <el-col class="photo" :span="24">
+      <el-avatar :size="80" :src="data.photo"></el-avatar>
+      <el-upload
+        :headers="headers"
+        :disabled="isEdit"
+        :on-change="onUpFile"
+        class="upload-demo"
+        ref="upload"
+        name="photo"
+        :show-file-list="false"
+        action="http://ttapi.research.itcast.cn/mp/v1_0/user/photo"
+        >
+        <el-button style="margin:20px;" slot="trigger" size="small" type="primary">更换头像</el-button>
+      </el-upload>
+    </el-col>
+  </el-row>
+    <!-- 表单 -->
+    <el-form :disabled="isEdit" :model="data" label-width="100px" class="demo-ruleForm" >
+      <el-form-item label="称昵" prop="name">
+        <el-input type="text" v-model="data.name" ></el-input>
+      </el-form-item>
+      <el-form-item label="简介" prop="intro">
+        <el-input type="text" v-model="data.intro" ></el-input>
+      </el-form-item>
+      <el-form-item label="手机号" prop="mobile">
+        <el-input type="text" v-model="data.mobile" ></el-input>
+      </el-form-item>
+      <el-form-item label="邮箱" prop="email">
+        <el-input type="text" v-model="data.email" ></el-input>
+      </el-form-item>
+    </el-form>
+</el-card>
 </template>
 
 <script>
 export default {
-
+  created () {
+    // 数据初始化
+    this.loadData()
+  },
+  data () {
+    return {
+      // 响应数据
+      data: {},
+      // 是否开启编辑
+      isEdit: true,
+      // 请求头
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`
+      }
+    }
+  },
+  methods: {
+    // 获取用户数据
+    loadData () {
+      // 发送请求
+      this.$axios.get('/user/profile')
+        .then(response => {
+          console.log(response.data)
+          this.data = response.data.data
+        }).catch(() => {
+          this.$message({
+            showClose: true,
+            message: '素材获取失败，请刷新',
+            type: 'warning'
+          })
+        })
+    },
+    // 编辑按钮
+    onEdit () {
+      this.isEdit = !this.isEdit
+    },
+    onUpFile (file, files) {
+      console.log(file, files)
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.box-card{
+  .photo{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+}
 </style>
--- src/views/home/content/Material.vue
+++ src/views/home/content/Material.vue
@@ -1,5 +1,10 @@
 <template>
-  <el-card class="box-card">
+  <el-card
+    v-loading="loading"
+    class="box-card"
+    element-loading-text="拼命加载中"
+    element-loading-spinner="el-icon-loading"
+    element-loading-background="rgba(0, 0, 0, 0.8)">
   <div slot="header" class="clearfix">
     <span>素材管理</span>
   </div>
@@ -9,12 +14,25 @@
       <el-radio-button :label="false">全部</el-radio-button>
       <el-radio-button :label="true">收藏</el-radio-button>
     </el-radio-group>
+    <el-upload
+    style="float:right;margin-top:-20px;"
+     :headers="headers"
+     :before-upload="onUpFile"
+     :on-success="onUpSuccess"
+     class="upload-demo"
+     ref="upload"
+     name="image"
+     :show-file-list="false"
+     action="http://ttapi.research.itcast.cn/mp/v1_0/user/images"
+     >
+     <el-button style="margin:20px;" slot="trigger" size="small" type="primary">上传素材</el-button>
+   </el-upload>
   </div>
   <el-row :gutter="20">
     <el-col style="margin-bottom:20px;" :xs="24" :sm="12" :md="8" :lg="6" :xl="4" v-for="(item, index) in data.results" :key="index" >
       <el-card :body-style="{ padding: '0px' }">
         <img :src="item.url" class="image">
-        <div v-show="!query.collect" class="bottom clearfix">
+        <div class="bottom clearfix">
           <!-- 星星是否收藏 -->
           <i @click="onChangeStar(item)" :class="{'el-icon-star-off' : !item.is_collected ,'el-icon-star-on':item.is_collected}"></i>
           <i @click="onDelete(item)" class="el-icon-delete-solid"></i>
@@ -42,15 +60,26 @@ export default {
         collect: false,
         page: null,
         per_page: null
+      },
+      // loading效果开关
+      loading: false,
+      // 请求头
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`
       }
     }
   },
   methods: {
     // 获取数据
     loadImage () {
+      // 开启loading效果
+      this.loading = true
+      // 请求数据
       this.$axios.get('/user/images', { params: this.query }).then(response => {
         // console.log(response.data.data)
         this.data = response.data.data
+        // 关闭loading效果
+        this.loading = false
       }).catch(() => {
         this.$message({
           showClose: true,
@@ -109,6 +138,16 @@ export default {
           message: '已取消删除'
         })
       })
+    },
+    // 上传前
+    onUpFile () {
+      this.loading = true
+      this.loadImage()
+    },
+    // 上传成功刷新
+    onUpSuccess () {
+      this.loading = false
+      this.loadImage()
     }
   },
   watch: {
